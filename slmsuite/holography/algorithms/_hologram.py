f99dba7e776ffd53b8d0d8fe5b23b11aa949f90e
Chris Panuski <cpanuski@gmail.com>
2026-05-24 14:34:08 -0400

    Bypass MRAF gpu<->cpu sync if mraf=False flag is passed.

--- slmsuite/holography/algorithms/_hologram.py
+++ slmsuite/holography/algorithms/_hologram.py
@@ -1495,7 +1495,9 @@ class Hologram(_HologramStats):
 
     def _mraf_helper_routines(self):
         # MRAF helper variables
-        if np == cp:
+        if not self.flags.get("mraf", True):
+            mraf_enabled = False
+        elif np == cp:
             mraf_enabled = np.isnan(np.sum(self.target))
         else:
             mraf_enabled = np.isnan(cp.sum(self.target).get())
